944f3fa3590a79333a95406b848bc93ea4ac4b92
Taj English <tajenglish@gmail.com>
2017-04-10 01:17:31 -0400

    Updated Soundcloud end points to HTTPS & 5th deploy

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,8 +5,8 @@ const API_KEY = "d6i0wruU7ddayTqrhwszluW0i9aNBlb1";
 
 //Api Routes
 module.exports = {
-  sadSongs: () => axios.get(`http://api.soundcloud.com/tracks?bpm[from]=50&bpm[to]=85&limit=5&filter=public&client_id=${API_KEY}`),
-  happySongs: () => axios.get(`http://api.soundcloud.com/tracks?bpm[from]=110&bpm[to]=130&limit=5&filter=public&client_id=${API_KEY}`),
-  angrySongs: () => axios.get(`http://api.soundcloud.com/tracks?bpm[from]=140&bpm[to]=300&limit=5&filter=public&client_id=${API_KEY}`)
+  sadSongs: () => axios.get(`https://api.soundcloud.com/tracks?bpm[from]=50&bpm[to]=85&limit=5&filter=public&client_id=${API_KEY}`),
+  happySongs: () => axios.get(`https://api.soundcloud.com/tracks?bpm[from]=110&bpm[to]=130&limit=5&filter=public&client_id=${API_KEY}`),
+  angrySongs: () => axios.get(`https://api.soundcloud.com/tracks?bpm[from]=140&bpm[to]=300&limit=5&filter=public&client_id=${API_KEY}`)
 
 }
